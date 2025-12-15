da050fe0d975949cc55085b8b57d2a44664183d8
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-12-15 12:25:50 +0000

    new book head and button

---
+++ books_to_try_mini/src/NewBookInput.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NewBookInput = () => {
+
+
+    return (
+        <form className="book-form">
+            <input input="text" area-label="Add New Book!"/>
+            <button> Add to List!</button>
+            </form>
+    )
+}
+export default NewBookInput;
