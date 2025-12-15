42ad7a7f43725f38197040cecade4f73f279f49b
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-12-15 13:25:00 +0000

    list function adde

--- books_to_try_mini/src/App.jsx
+++ books_to_try_mini/src/App.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react'
 import './App.css'
 import NewBookInput from './NewBookInput.jsx'
+
+
+
 function App() {
-  // const [count, setCount] = useState(0)
+  
+  
+  const [booksToRead, setBooksToRead] = useState([]);
+
+  const addBook = (book) => {
+    setBooksToRead([...booksToRead, book]);
+  };
 
   return (
     <div className='App'>
       <h1>Book List!</h1>
-      <NewBookInput />
+      <NewBookInput addBook={addBook}/>
+      <ul className='book-list'>
+        {booksToRead.map((book, index) => {
+        return <li key={index}>{book}</li>
+        })}
+      </ul>
     </div>
   )
 }
--- books_to_try_mini/src/NewBookInput.jsx
+++ books_to_try_mini/src/NewBookInput.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NewBookInput = () => {
+const NewBookInput = (props) => {
+    const [book, setBook] = useState("");
+
+    const updatedBook = (event) => {
+    setBook(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.addBook(book);
+        setBook("");
+    }
 
 
     return (
-        <form className="book-form">
-            <input input="text" area-label="Add New Book!"/>
+        <form className="book-form" onSubmit={handleSubmit}>
+            <input 
+            input="text" 
+            area-label="Add New Book!" 
+            value={book} 
+            onChange={updatedBook}
+            />
             <button> Add to List!</button>
             </form>
     )
