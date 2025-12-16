672bf8bf4719ae5d29ada6e91d6e5db602773538
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-12-16 18:50:43 +0000

    Fix paths for GitHub Pages

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/books_to_read_mini/',
+  base: '/books_to_try_mini/',
   plugins: [react()],
 })
